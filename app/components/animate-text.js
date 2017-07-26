21ccd1e11dcf91044a8f733191228e590da3d573
mulatya <emmanuelmulatya8@gmail.com>
2017-07-26 09:46:05 +0300

    yeh

---
+++ app/components/animate-text.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+//   ('.txt').html(function(i, html) {
+//   var chars = trim(html).split("");
+//
+//   return '<span>' + chars.join('</span><span>') + '</span>';
+// });
+});
